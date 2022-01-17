b372296e6601450f3e91a8d7adee6f0d76ed6b91
ArtemLytvynenkoa <chpart355@gmail.com>
2022-01-17 18:15:10 +0200

    adds some logic

--- src/javascript/Pagination/renderPaginationMarkup.js
+++ src/javascript/Pagination/renderPaginationMarkup.js
@@ -1,18 +1,21 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
+import './pagination.scss';
 
 const API_KEY = 'c7ed46652640bc5a91d5a4e73d915c28';
+
+let currentPage = 1;
 
 const options = {
   totalItems: 2000, 
   itemsPerPage: 1,
-  visiblePages: 10,
+  visiblePages: 5,
   page: 1,
   centerAlign: false,
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
   template: {
-    page: '<button class="tui-page-btn">{{page}}</button>',
+    page: '<button class="tui-page-btn" data-number="{{page}}">{{page}}</button>',
     // '<a href="#" class="tui-page-btn">{{page}}</a>',
     currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
@@ -24,10 +27,68 @@ const options = {
         '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip" >' +
         '<span class="tui-ico-ellip">...</span>' +
       '</a>'
   }
 };
 
 const pagination = new Pagination('pagination', options);
+
+
+const currentButton = document.querySelector('.tui-pagination');
+
+console.log(currentPage)
+
+currentButton.addEventListener('click', event => {
+  const { className } = event.target;
+
+  console.dir(event.currentTarget.children[2].className)
+  
+  console.dir(event.target);
+  // if (className.includes("tui-ico-first")) {
+  //   currentPage = 1;
+  //   console.log(currentPage)
+  //   return
+  // };
+
+  // if (className.includes("tui-ico-last")) {
+  //   currentPage = options.totalItems;
+  //   console.log(currentPage)
+  //   return
+  // };
+
+  // if (className.includes("tui-ico-next")) {
+  //   currentPage += 1;
+  //   console.log(currentPage)
+  //   return
+  // };
+
+  // if (className.includes("tui-ico-prev")) {
+  //   currentPage -= 1;
+  //   console.log(currentPage)
+  //   return
+  // };
+
+  // if (className.includes("tui-ico-prev")) {
+  //   currentPage -= 1;
+  //   console.log(currentPage)
+  //   return
+  // };
+
+
+  const { children } = event.currentTarget;
+  for (let i = 0; i < event.currentTarget; i += 1) {
+    
+  }
+  
+  // console.log(children)
+  // event.currentTarget.children.forEach(child => {
+  //   if (child.className.includes("tui-is-selected")) {
+  //     return currentPage = +child.dataset.number;
+  // }
+  // })
+
+  // currentPage = +event.target.dataset.number;
+  console.log(currentPage)
+})
