c1c7a56fec30ee90d1f7c5598cb1831de09f29bc
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-01-21 13:23:30 +0200

    Create modal.js

---
+++ src/javascript/modal.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector('[data-modal]');
+const openModalBtn = document.querySelector('[data-modal-open]');
+const closeModalBtn = document.querySelector('[data-modal-close]');
+
+openModalBtn.addEventListener("click", toggleModal);
+closeModalBtn.addEventListener("click", toggleModal);
+
+function toggleModal () {
+    modal.classList.toggle("is-hidden");
+}
